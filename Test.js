dc262dbfc3612a821ca199ea076671b26858e797
LenzA99 <71934563+LenzA99@users.noreply.github.com>
2021-11-06 14:11:08 -0400

    Button testing

---
+++ Test.js
@@ -0,0 +1,8 @@
+
+$('div .checkbox').click(function () {                 
+     checkedState = $(this).attr('checked');
+$(this).parent('div').children('.checkbox:checked').each(function () {
+    $(this).attr('checked', false);
+});
+$(this).attr('checked', checkedState);
+});
