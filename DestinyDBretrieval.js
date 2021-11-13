a6b24b3c3894cc876152b8567afee61c32e57302
LenzA99 <71934563+LenzA99@users.noreply.github.com>
2021-11-13 14:03:59 -0500

    update php and javascript
    php works with database just need output

--- DestinyDBretrieval.js
+++ DestinyDBretrieval.js
@@ -1,10 +1,21 @@
-$('div .checkbox').click(function () { 
-    checkedState = $(this).attr('checked');
-     $(this).parent('div').children('.checkbox:checked').each(function () {
-         $(this).attr('checked', false);
-     });
-     $(this).attr('checked', checkedState);
-});
-
-SELECT columns FROM table WHERE MATCH( columns ) AGAINST( "search term" );
-SELECT * FROM armor,ghost,weapon WHERE MATCH( name ) AGAINST( "search term" );  
+function ajsearch() {
+  // (A) GET SEARCH TERM
+  var data = new FormData();
+  data.append("search", document.getElementById("search").value);
+  data.append("ajax", 1);
+ 
+  // (B) AJAX SEARCH REQUEST
+  fetch("searchdb.php", { method:"POST", body:data })
+  .then(res => res.json()).then((results) => {
+    var wrapper = document.getElementById("results");
+    if (results.length > 0) {
+      wrapper.innerHTML = "DestinyDB.html";
+      for (let res of results) {
+        let line = document.createElement("div");
+        line.innerHTML = `${res["Armor_Name"]} - ${res["Weapon_Name"]}- ${res["Ghost_Name"]}`;
+        wrapper.appendChild(line);
+      }
+    } else { wrapper.innerHTML = "No results found"; }
+  });
+  return false;
+}
