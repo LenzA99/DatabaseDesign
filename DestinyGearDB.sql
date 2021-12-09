cbbd38a196f8e850756f3cc1b8ebb66c9b3b8bdd
LenzA99 <71934563+LenzA99@users.noreply.github.com>
2021-12-09 12:22:29 -0500

    update sql

--- DestinyGearDB.sql
+++ DestinyGearDB.sql
@@ -23,17 +23,12 @@ DROP TABLE IF EXISTS `armor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `armor` (
-  `Armor_Name` varchar(45) NOT NULL,
-  `Armor_Pwr_Max` double DEFAULT NULL,
-  `Armor_Type` varchar(45) DEFAULT NULL,
-  `Class_ID` varchar(45) NOT NULL,
+   `Name` varchar(45) NOT NULL,
+  `Power_Max` double DEFAULT NULL,
+  `Type` varchar(45) DEFAULT NULL,
+  `Class` varchar(45) DEFAULT NULL,
   `Rarity_ID` varchar(45) NOT NULL,
-  PRIMARY KEY (`Armor_Name`),
-  UNIQUE KEY `Armor_Name_UNIQUE` (`Armor_Name`),
-  KEY `class_id` (`Class_ID`) /*!80000 INVISIBLE */,
-  KEY `fk_armor_rarity1_idx` (`Rarity_ID`),
-  CONSTRAINT `class_id` FOREIGN KEY (`Class_ID`) REFERENCES `class` (`Class_ID`),
-  CONSTRAINT `rarity_id` FOREIGN KEY (`Rarity_ID`) REFERENCES `rarity` (`Rarity_ID`)
+  PRIMARY KEY (`Name`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -47,28 +42,6 @@ INSERT INTO `armor` VALUES ('Nezarec\'s' Sin',1326,'Warlock','Exotic','Helmet');
 /*!40000 ALTER TABLE `armor` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `class`
---
-
-DROP TABLE IF EXISTS `class`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `class` (
-  `Class_ID` varchar(45) NOT NULL,
-  PRIMARY KEY (`Class_ID`),
-  UNIQUE KEY `Class_ID_UNIQUE` (`Class_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `class`
---
-
-LOCK TABLES `class` WRITE;
-/*!40000 ALTER TABLE `class` DISABLE KEYS */;
-/*!40000 ALTER TABLE `class` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `ghost`
@@ -78,11 +51,9 @@ DROP TABLE IF EXISTS `ghost`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ghost` (
-  `Ghost_Name` varchar(45) NOT NULL,
+  `Name` varchar(45) NOT NULL,
   `Rarity_ID` varchar(45) NOT NULL,
-  PRIMARY KEY (`Ghost_Name`),
-  KEY `rariy_id_ghost` (`Rarity_ID`),
-  CONSTRAINT `rarity__ghost` FOREIGN KEY (`Rarity_ID`) REFERENCES `rarity` (`Rarity_ID`)
+  PRIMARY KEY (`Name`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -96,28 +67,7 @@ INSERT INTO `ghost` VALUES ('Cosmographicum Shell','Exotic');
 /*!40000 ALTER TABLE `ghost` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `rarity`
---
 
-DROP TABLE IF EXISTS `rarity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `rarity` (
-  `Rarity_ID` varchar(45) NOT NULL,
-  PRIMARY KEY (`Rarity_ID`),
-  UNIQUE KEY `Rarity_ID_UNIQUE` (`Rarity_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `rarity`
---
-
-LOCK TABLES `rarity` WRITE;
-/*!40000 ALTER TABLE `rarity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rarity` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `weapon`
@@ -127,15 +77,12 @@ DROP TABLE IF EXISTS `weapon`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `weapon` (
-  `Weapon_Name` varchar(45) NOT NULL,
-  `Weapon_Pwr_Max` double DEFAULT NULL,
-  `Weapon_Dmg_Typ` varchar(45) DEFAULT NULL,
-  `Weapon_Type` varchar(45) DEFAULT NULL,
+  `Name` varchar(45) NOT NULL,
+  `Power_Max` double DEFAULT NULL,
+  `Type` varchar(45) DEFAULT NULL,
+  `Class` varchar(45) DEFAULT NULL,
   `Rarity_ID` varchar(45) NOT NULL,
-  PRIMARY KEY (`Weapon_Name`),
-  UNIQUE KEY `Weapon_Name_UNIQUE` (`Weapon_Name`),
-  KEY `rarity_id_weapon` (`Rarity_ID`),
-  CONSTRAINT `rarity_id_weapon` FOREIGN KEY (`Rarity_ID`) REFERENCES `rarity` (`Rarity_ID`)
+  PRIMARY KEY (`Name`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -143,8 +90,8 @@ CREATE TABLE `weapon` (
 -- Dumping data for table `weapon`
 --
 
-LOCK TABLES `weapon` WRITE;
-/*!40000 ALTER TABLE `weapon` DISABLE KEYS */;
+LOCK TABLES `wepon` WRITE;
+/*!40000 ALTER TABLE `ghost` DISABLE KEYS */;
 INSERT INTO `weapon` VALUES ('Ager\'s Scepter',1330,'Stasis','Trace Rifle','Exotic');
 /*!40000 ALTER TABLE `weapon` ENABLE KEYS */;
 UNLOCK TABLES;
